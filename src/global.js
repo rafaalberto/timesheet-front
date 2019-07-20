f1007ceb2c48828049e570d458a0b2d6069bb799
Rafael Alberto <rafael.alberto1703@gmail.com>
2019-07-20 10:52:50 -0300

    atualizando base url

--- src/global.js
+++ src/global.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 
 export const userKey = '__api_user'
-export const baseApiUrl = 'http://localhost:8080'
+export const baseApiUrl = 'http://localhost:8080/timesheet-api'
 
 export function showError(error) {
     if(error && error.response && error.response.data) {
